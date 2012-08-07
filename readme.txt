678d9e606fcfc959feba53b23c42b3f621e8d282
cgrymala <cgrymala@59dbafea-b281-44cb-bbdb-86e0dafc49a6>
2012-08-07 00:48:57 +0000

    Updated version number
    git-svn-id: svn+ssh://ftp.ten-321.com/home/cgrymala/svn/sau-campus-directory/trunk@20 59dbafea-b281-44cb-bbdb-86e0dafc49a6

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: cgrymala, smkeith
 Tags: contact, employees, staff, directory, faculty
 Requires at least: 3.2
-Tested up to: 3.4
-Stable tag: 1.0.1a
+Tested up to: 3.4.1
+Stable tag: 1.0.4a
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- sau-campus-directory.php
+++ sau-campus-directory.php
@@ -3,7 +3,7 @@
 Plugin Name: SAU Magnolia Campus Directory
 Plugin URI: http://web.saumag.edu/directory/category/college-of-business
 Description: Displays the directory information in a custom template
-Version: 1.0.1a
+Version: 1.0.4a
 Author: Curtiss Grymala and Shelley Keith
 Author URI: http://ten-321.com
 License: GPL2
