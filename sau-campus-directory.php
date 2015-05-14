b11db9dfd7a6822cdaa5948fdbc384564ae17d85
Curtiss Grymala <curt_grymala@yahoo.com>
2015-05-14 12:46:42 -0400

    Increment version number for future development

--- sau-campus-directory.php
+++ sau-campus-directory.php
@@ -3,7 +3,7 @@
 Plugin Name: SAU Magnolia Campus Directory
 Plugin URI: http://web.saumag.edu/directory/category/college-of-business
 Description: Displays the directory information in a custom template
-Version: 1.0.5a
+Version: 1.1
 Author: Curtiss Grymala and Shelley Keith
 Author URI: http://ten-321.com
 License: GPL2
